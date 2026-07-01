75cf287a9401464bfa11fdfbebb363973e96f0a9
aleh4man <amfcomputing@gmail.com>
2026-07-01 17:01:47 +0300

    initial commit

---
+++ src/main/java/org/aleh4min/labtask1/Main.java
@@ -0,0 +1,11 @@
+package org.aleh4min.labtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
